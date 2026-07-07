9934342bb084fed500a6cbc2f1f32db44c9c86c4
mauryapankaj1999 <pankajmaurya4610@gmail.com>
2026-07-07 18:15:36 +0530

    error solved

--- src/hooks/useModules.ts
+++ src/hooks/useModules.ts
@@ -2,9 +2,7 @@ import { getModulesByCourse } from "@/services/moduleApi";
 import { useQuery } from "@tanstack/react-query";
 
 
-export const useModules = (
-  courseId
-) => {
+export const useModules = (courseId:string) => {
   return useQuery({
     queryKey: ["modules", courseId],
     queryFn: () =>
