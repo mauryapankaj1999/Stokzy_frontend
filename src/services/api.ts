a535e56043a0d84c9dcd1331d05c9e0baf8cd25c
mauryapankaj1999 <pankajmaurya4610@gmail.com>
2026-07-13 14:46:44 +0530

    url chnage

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://stokzy-backend.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://stokzy-backend.onrender.com/api",
 });
 
 api.interceptors.request.use(
